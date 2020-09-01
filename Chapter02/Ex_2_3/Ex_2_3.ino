043ca8c2c69d348763745401d74f168917f4f114
irostub <irostub@gmail.com>
2020-09-01 14:55:01 +0900

    Update code of example 2.3

--- Chapter02/Ex_2_3/Ex_2_3.ino
+++ Chapter02/Ex_2_3/Ex_2_3.ino
@@ -4,17 +4,30 @@
   컴퓨터로부터 시리얼 통신을 통하여 데이터 수신하기의 예제입니다
 
 */
-const int ledPin = 13;
-int blinkNumber = 0;
+const int ledPin = 13;        //제어할 핀 번호
+int blinkNumber = 0;          //핀 점멸 회수제어 변수
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(ledPin.OUTPUT);
+  pinMode(ledPin,OUTPUT);     //13번 핀에 대해 output으로 설정
 }
 
 void loop() 
 {
-  if(Serial.available()){
-    char val = Serial.read();
+  if(Serial.available())
+  {
+    char val = Serial.read(); //시리얼 모니터로부터 값 읽기 block됨
+    if(isDigit(val))          //0~9의 숫자이면 True 반환, isDigit()함수
+    {         
+      blinkNumber=(val-'0');  //ASCII 0~9를 DEC 0~9로 변환하기 위해 ASCII기준 문자 0값 뺄셈
+    }
   }
+  for(char i = 0; i < blinkNumber; i++)
+  {
+    digitalWrite(ledPin, HIGH); //led on
+    delay(200);
+    digitalWrite(ledPin,LOW);   //led off
+    delay(200);
+  }
+  blinkNumber = 0;
 }
