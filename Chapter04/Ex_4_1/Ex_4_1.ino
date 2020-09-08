51f66a07a719097e1a85f8adf11844fda8452fa4
irostub <irostub@gmail.com>
2020-09-08 20:38:44 +0900

    Add code of example 4.1

---
+++ Chapter04/Ex_4_1/Ex_4_1.ino
@@ -0,0 +1,24 @@
+/*
+
+  예제 4.1
+  LED 점멸의 예제입니다
+
+*/
+const int ledA = 3;
+const int ledB = 5;
+
+void setup() 
+{
+  pinMode(ledA, OUTPUT);
+  pinMode(ledB, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledA, HIGH);
+  digitalWrite(ledB, LOW);
+  delay(100);
+  digitalWrite(ledA, LOW);
+  digitalWrite(ledB, HIGH);
+  delay(100);
+}
