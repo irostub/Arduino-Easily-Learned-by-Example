07e9ae93628d7bf43423da10df72b903e90de9f0
irostub <irostub@gmail.com>
2020-09-08 20:39:15 +0900

    Add code of example 4.2.2

---
+++ Chapter04/Ex_4_2_2/Ex_4_2_2.ino
@@ -0,0 +1,31 @@
+/*
+
+  예제 4.2.2
+  LED 밝기 조절에 대한 예제의 2번 응용문제 코드입니다
+
+*/
+const int leds[5] = {3, 5, 6, 9, 10};
+int ledBrightness[5] = {255, 255, 255, 255, 255};
+boolean switcher = 1;
+
+void setup() 
+{
+  for(int i = 0; i < 5; i++)
+  {
+    analogWrite(leds[i],ledBrightness[i]);
+  }
+}
+
+void loop()
+{
+  for(int i = 0; i < 5; i++)
+  {
+    for(int j = 0; j < 255; j++){
+      if(switcher)ledBrightness[i]--;
+      else ledBrightness[i]++;
+      analogWrite(leds[i],ledBrightness[i]);
+      delay(10);
+    }
+  }
+  switcher = !switcher;
+}
