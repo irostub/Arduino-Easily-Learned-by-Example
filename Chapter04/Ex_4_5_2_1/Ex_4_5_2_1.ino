1ab28a8171fe1d107839f683d0b3c511a08a8002
irostub <irostub@gmail.com>
2020-09-21 11:51:11 +0900

    Add code of example 4.5.2.1

---
+++ Chapter04/Ex_4_5_2_1/Ex_4_5_2_1.ino
@@ -0,0 +1,72 @@
+/*
+
+  예제 4.5.2
+  4 Digit FND에 1초마다 증가하는 0~9999 숫자 표시하기 예제입니다.
+ 
+*/
+const byte number[10] = {
+  B00111111,
+  B00000110,
+  B01011011,
+  B01001111,
+  B01100110,
+  B01101101,
+  B01111101,
+  B00000111,
+  B01111111,
+  B01101111
+};
+
+const byte digitNumber[4] = {13,12,11,10};
+int count = 0;
+int value[4];
+int digitSelect;
+long sampleTime;
+int count5ms;
+
+void setup(){
+  for(int i=2; i<=13; ++i){
+    pinMode(i, OUTPUT);
+  }
+  for(int i=10; i<=13; ++i){
+    digitalWrite(i,HIGH);
+  }
+}
+
+void loop(){
+  sampleTime = millis();
+  fndDisplay(digitSelect, value[digitSelect-1]);
+  ++digitSelect;
+  if(digitSelect>=5) digitSelect=1;
+
+  if(count>=9999) count = 0;
+  else{
+    while(millis()-sampleTime<5);
+    ++count5ms;
+    if (count5ms>20){
+      ++count;
+
+      value[3] = count/1000;
+      value[2] = (count - (value[3]*1000))/100;
+      value[1] = (count - (value[3]*1000)-(value[2]*100))/10;
+      value[0] = (count - (value[3]*1000)-(value[2]*100)-value[1]*10);     
+
+      count5ms = 0;
+    }
+  }
+  }
+
+void fndDisplay(int digit, int displayValue){
+  boolean bitValue;
+  for(int i=1; i<=4; ++i){
+    digitalWrite(digitNumber[i-1],HIGH);
+  }
+  for(int i=0; i<=7; ++i){
+    bitValue = bitRead(number[displayValue],i);
+    digitalWrite(i+2,bitValue);
+  }
+  for(int i=1; i<=4; ++i){
+    if(digit==i) digitalWrite(digitNumber[i-1],LOW);
+    else digitalWrite(digitNumber[i-1],HIGH);
+  }
+}
