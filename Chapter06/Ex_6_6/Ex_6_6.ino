21e4bdaa7f343c90e13e4be8562f87ea9adce585
irostub <irostub@gmail.com>
2020-10-01 03:17:04 +0900

    Add code of example 6.6

---
+++ Chapter06/Ex_6_6/Ex_6_6.ino
@@ -0,0 +1,37 @@
+/*
+
+  예제 6.6
+  마이크 입력 실험 예제입니다.
+ 
+*/
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+char soundInputPin = 0;
+
+void setup() {
+ lcd.begin(16,2);
+ lcd.print("MEP-6 Ex 6.6");
+ lcd.setCursor(0,1);
+ lcd.print("MIC level");
+ delay(3000);
+ lcd.clear();
+ lcd.setCursor(0,0);
+ lcd.print("ADC :");
+ lcd.setCursor(0,1);
+ lcd.print("MIC :");
+ lcd.setCursor(15,1);
+
+}
+
+void loop() {
+  int soundInput = analogRead(soundInputPin);
+  int soundLevel = map(soundInput, 50, 900, 0, 100);
+  lcd.setCursor(6,0);
+  lcd.print(soundInput);
+  lcd.setCursor(6,1);
+  lcd.print(soundLevel);
+  
+  delay(1000);
+}
