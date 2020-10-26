eead64c0408854ba00451a36a08ed727db2123e1
irostub <irostub@gmail.com>
2020-10-26 23:12:18 +0900

    Add code of example 9.5

---
+++ Chapter09/Ex_9_5/Ex_9_5.ino
@@ -0,0 +1,31 @@
+const char trigPin = 13;
+const char echoPin = 12;
+
+int pulseWidth;
+int distance;
+int distanceOld;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  digitalWrite(trigPin,LOW);
+}
+
+void loop() {
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+
+  pulseWidth = pulseIn(echoPin,HIGH);
+  distance = pulseWidth /58;
+
+  if(distance <=200||distance >=2){
+    if(distance != distanceOld){
+      Serial.print(distance);
+      Serial.println(" cm");
+    }
+  }
+  distanceOld = distance;
+  delay(100);
+}
